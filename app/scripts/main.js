eeb9746a74470983014fdf3d5ad0b2a09c1a7119
mciastek <miroslaw.ciastek@gmail.com>
2013-12-21 16:23:52 +0100

    First steps - add full window size to .tab-wrap

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,24 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+	
+
+
+	var fullWindow = function(width, height) {
+		this.width = $(".tab-wrap").css("width", width);
+		this.height = $(".tab-wrap").css("height", height);
+
+	}
+
+	var windowWidth = $(window).innerWidth();
+	var windowHeight = $(window).innerHeight();
+
+	var tabWrap = new fullWindow(windowWidth, windowHeight);
+
+	$(window).resize(function(event) {
+		var windowWidth = $(window).innerWidth();
+		var windowHeight = $(window).innerHeight();
+
+		var tabWrap = new fullWindow(windowWidth, windowHeight);
+	});
+
+
+});
