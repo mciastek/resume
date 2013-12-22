9a410236ffb9da2704e9c09173413eb72b8fbe4e
mciastek <miroslaw.ciastek@gmail.com>
2013-12-22 17:26:27 +0100

    Added feature for checking window coords

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -3,8 +3,8 @@ $(document).ready(function() {
 
 	// full window tab
 	var fullWindow = function(width, height) {
-		this.width = $(".tab-wrap").css("width", width);
-		this.height = $(".tab-wrap").css("height", height);
+		this.width = $('.tab-wrap').css('width', width);
+		this.height = $('.tab-wrap').css('height', height);
 
 	}
 	// window size
@@ -16,24 +16,20 @@ $(document).ready(function() {
 
 	// simple scrollTo function
 	function scrollTo(href) {
-		$("html, body").animate({
+		$('html, body').animate({
 			scrollTop: $(href).offset().top},
-			"slow");
-
-		if(href == "#skills") {
-			chartInit();
-		}
+			'slow');
 	}
 
 	// navigation scrollTo
-	$(".dot-item a").click(function(e) {
+	$('.dot-item a').click(function(e) {
 		e.preventDefault();
 
-		scrollTo($(this).attr("href"));
+		scrollTo($(this).attr('href'));
 
 	});
 
-	var $tabWrap = $(".tab-wrap");
+	var $tabWrap = $('.tab-wrap');
 	var tabOffsetTop = [];
 	var tabOffsetBottom = [];
 
@@ -49,29 +45,58 @@ $(document).ready(function() {
 	}
 	addTabCoords();
 
-
+	var timer;
 	// window scroll event
 	$(window).scroll(function(event) {
-		// remove .active classes
-		$(".dot-item").removeClass('active');
+		
+		// check if user finshed scrolling
+		if(timer){
+			clearTimeout(timer);  
+		}
+		timer = setTimeout(function(){
 
-		$(".tab-wrap").each(function(index, el) {
-			// window is between .tab-wrap add proper class to nav item
-			if ($(window).scrollTop() >= tabOffsetTop[index] && $(window).scrollTop() < tabOffsetBottom[index]) {
-				$(".dot-item").eq(index).addClass('active');
-			}
-		});
+			// remove .active classes
+			$('.dot-item').removeClass('active');
 
+			$('.tab-wrap').each(function(index, el) {
+				var $getTabId = $('.tab-wrap').eq(index).attr('id');
+				// window is between .tab-wrap add proper class to nav item
+				if ($(window).scrollTop() >= tabOffsetTop[index] && $(window).scrollTop() < tabOffsetBottom[index]) {
+					$('.dot-item').eq(index).addClass('active');
 
-		// if user scroll to skills init charts
-		if($(this).scrollTop() == $("#skills").offset().top) chartInit();
+					//console.log($('.tab-wrap').eq(index).attr('id'));
+					switch ($getTabId) {
+
+						case 'hello': {
+						move = 0;
+						}
+						break;
+
+						case 'exp': {
+						showExpList();
+						move = 1;
+						}
+						break;
+
+						case 'skills': {
+						chartInit();
+						move = 2;
+						}
+						break;
+					}
+				}
+			});
+		   // set timeout
+		}, 200);
 
 	});
+
+	
 
 	// add .tab-wrap ids to array
 	var tabWrapIds = [];
 		for(var i=0; i < $tabWrap.length; i++) {
-			tabWrapIds.push($tabWrap.eq(i).attr("id"));
+			tabWrapIds.push($tabWrap.eq(i).attr('id'));
 	}
 
 	var move = 0;
@@ -92,7 +117,7 @@ $(document).ready(function() {
 				} else {
 					move--;
 				}
-			scrollTo("#"+tabWrapIds[move]);
+			scrollTo('#'+tabWrapIds[move]);
 			
 		}
 
@@ -104,21 +129,26 @@ $(document).ready(function() {
 				} else {
 					move++;	
 				}
-			scrollTo("#"+tabWrapIds[move]);
+			scrollTo('#'+tabWrapIds[move]);
 			
 		}
 	});
 
-
+	function showExpList() {
+	// experience list fade in
+	$('#exp-list li').each(function(index){
+		$(this).delay(index*400).animate({opacity: 1}, 400);
+	});
+	}
 
 	function chartInit() {
     //create instance
 	    $('.chart').easyPieChart({
 	    	animate: 2000,
-	    	barColor: "#e6311c",
+	    	barColor: '#e6311c',
 	    	trackColor: false,
 	    	scaleColor: false,
-	    	lineCap: "square",
+	    	lineCap: 'square',
 	    	lineWidth: 12
 	    });
 	};
